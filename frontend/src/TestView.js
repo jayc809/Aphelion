45139ba25c2daa363374d9bfd758234e940d7aba
jayc809 <81512286+jayc809@users.noreply.github.com>
2022-06-05 05:56:15 -0700

    fixed hold bar offset on miss

--- frontend/src/TestView.js
+++ frontend/src/TestView.js
@@ -11,9 +11,9 @@ const TestView = () => {
 
     return (
         <div>
-            <div style={{position: "absolute", zIndex: 10, height: "100%", width: "100%", overflow: "hidden"}}>
-                
-            </div>
+            {/* <Tile type={"middle-right"} tileSpeed={1.3} targetTime={0} elapseBeatCount={3} elapseTime={3} onMount={(type, targetTime, controller) => {}} onMiss={(type, targetTime) => {}} updateScoreAndCombo={(accuracy) => {}} id={100}></Tile> */}
+            <HoldTile type={"right"} tileSpeed={1.3} targetTime={0} elapseBeatCount={3} elapseTime={3} onMount={(type, targetTime, controller) => {}} onMiss={(type, targetTime) => {}} updateScoreAndCombo={(accuracy) => {}} id={100}/>
+            <Platform></Platform>
         </div>
     )
 }
