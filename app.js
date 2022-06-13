6e67c1c2a05a182c4ecdc068ada9bb2c6a776d83
jayc809 <81512286+jayc809@users.noreply.github.com>
2022-06-13 00:20:54 -0700

    try deploying to heroku

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const app = express()
+const cors = require('cors');
 const path = require("path")
+
 const port = process.env.PORT || 4000
 
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static("./frontend/build"))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
-    })
-}
+app.use(cors())
+app.use(express.static("./frontend/build"))
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
+})
 
 app.listen(port, (err) => {
     if (err) return console.log(err)
