735fd6821ff90afccf8394cb1f0050cd7837ef4c
jayc809 <81512286+jayc809@users.noreply.github.com>
2022-06-29 02:59:17 -0700

    try eb

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use(express.static("./frontend/build"))
 app.use(express.static("./public"))
 
 app.get("/", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
-    res.sendFile(path.resolve(__dirname, "./frontend/public/index.html"))
+    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
 })
 
 app.get("/animation", (req, res) => {
