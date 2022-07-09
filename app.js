34f5b3597ecd20479db733aace3f6d807d83e3ad
jayc809 <81512286+jayc809@users.noreply.github.com>
2022-07-08 20:43:48 -0700

    test

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use(express.static("./frontend/build"))
 app.use(express.static("./public"))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
+    console.log(__dirname)
+    // res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
+    // res.sendFile(path.resolve(__dirname, "./index.html"))
+    res.send("yee")
 })
 
 app.get("/animation", (req, res) => {
