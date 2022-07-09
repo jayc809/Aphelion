68918b3e87d7649b8ba8251afecbd93f3a3b98ce
jayc809 <81512286+jayc809@users.noreply.github.com>
2022-07-08 21:23:29 -0700

    test5

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(cors())
-app.use(express.static("./frontend/build"))
 // app.use(express.static("./public"))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./frontend/index.html"))
+    app.use(express.static("./frontend/build"))
+    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
     // res.sendFile(path.resolve(__dirname, "./index.html"))
 })
 
